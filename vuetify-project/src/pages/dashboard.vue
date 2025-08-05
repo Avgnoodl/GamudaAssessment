7925e4adbd42626bd671bb6a50b9ae33dc24a2a4
Sebastian Kam <jkam0003@student.monash.edu>
2025-08-05 10:26:56 +0800

    little dashboard to verify setup

---
+++ vuetify-project/src/pages/dashboard.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col>
+        <h1>Dashboard</h1>
+        <p>Welcome to your dashboard page.</p>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+</style>
